664242059aea8a305219f1c4ff0c5868e3d1147f
some-6ude <102697068+some-6ude@users.noreply.github.com>
2022-06-01 09:00:50 -0600

    test custom code

--- injectables/customblookloading/factoryHook.js
+++ injectables/customblookloading/factoryHook.js
@@ -1,10 +1,3 @@
 if (blooketUtility.prefs.infiniteFactory.value) {
-  Object.defineProperty(Object.prototype, "timerInterval", {
-    get: function () {
-      return -1;
-    },
-    set: function (d) {
-      clearInterval(d);
-    }
-  });
+  console.log("hello world from a blooket utility script");
 }
