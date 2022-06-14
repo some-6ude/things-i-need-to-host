ae6fd3a1ebad963a547ca57af2cba08ac176e7a4
karim <no>
2022-06-14 17:10:20 -0600

    organise

---
+++ blooket/multitool/main.js
@@ -0,0 +1,15 @@
+// stack overflow code :]
+function loadscript(url) {
+    var script = document.createElement("script");  // create a script DOM node
+    script.src = url;  // set its src to the provided URL       
+    document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
+var hacc = prompt("eyo which hax? (all lowercase and no spaces")
+
+
+if (hacc = "fossilhack") {
+    document.write("el pepe")
+}
+if (hacc = "addtokens") {
+    loadscript("https://raw.githubusercontent.com/rxzyx/Blooket-Hacks/main/Default%20Scripts/Add%20Tokens.js")
+}
